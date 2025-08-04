19777a712387be7c311a6b3482d6d097dffa348f
hammer43 <rm97779@gmail.com>
2025-08-04 01:55:25 -0400

    Fix: Passed onSelect prop to Sidebar

--- book-mvp/pages/index.tsx
+++ book-mvp/pages/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import ReadingPane from '../components/ReadingPane';
+import { Prompt } from '../types'; // if you have this type defined
 
 export default function Home() {
+  const [selectedPrompt, setSelectedPrompt] = useState<Prompt | null>(null);
+
   return (
     <div className="flex h-screen">
-      <Sidebar />
-      <ReadingPane />
+      <Sidebar onSelect={setSelectedPrompt} />
+      <ReadingPane prompt={selectedPrompt} />
     </div>
   );
 }
